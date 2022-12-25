1508e41efca5f60b0a09f09adb1890b72e477004
Tran Quang <taquang.hskx.2000@gmail.com>
2022-12-25 19:21:23 +0700

    feat: create menu

---
+++ src/components/header/Menu.tsx
@@ -0,0 +1,121 @@
+import { lazy, useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { fetchGenres } from '~/services/movie'
+import { IGenre } from '~/types/movies'
+import AppConfig from '../../../AppConfig'
+
+const Image = lazy(() => import('~/components/base/Image'))
+const MenuMobile = lazy(() => import('~/components/header/MenuMobile'))
+
+const Menu = () => {
+  const [menus, setMenus] = useState<IGenre[]>()
+  const [isLoading, setIsLoading] = useState(false)
+  const [idActive, setIdActive] = useState<number | null>(null)
+  const [isShowMenuMobile, setIsShowMenuMobile] = useState(false)
+
+  useEffect(() => {
+    getGenres()
+    return () => {
+      setMenus([])
+    }
+  }, [])
+
+  const getGenres = async () => {
+    setIsLoading(true)
+    try {
+      const { genres } = await fetchGenres()
+      setMenus(genres)
+    } catch (error: any) {
+      toast.error(error.status_message || 'Something went wrong!')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const handleActive = (id: number) => {
+    setIdActive(id)
+    return {
+      color: 'rgb(20 184 166)',
+    }
+  }
+
+  if (isLoading) return <MenuSkeleton />
+
+  if (!menus) return <></>
+
+  return (
+    <>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
+        strokeWidth={1.5}
+        stroke='currentColor'
+        className='w-6 h-6 cursor-pointer absolute right-5 top-5 text-white md:hidden'
+        onClick={() => setIsShowMenuMobile(true)}>
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5'
+        />
+      </svg>
+
+      <header className='flex-1 justify-end md:justify-center fixed top-6 z-10 inset-x-0 mx-auto hidden md:flex'>
+        <nav className='pointer-events-auto'>
+          <ul className='flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur'>
+            <li>
+              <Image
+                src='/assets/images/logo.png'
+                alt='This is logo'
+                className='ring-4 ring-gray-200 ring-opacity-20 mt-1'
+                width='32'
+              />
+            </li>
+            <li>
+              <NavLink
+                to='/'
+                style={({ isActive }) => (isActive ? handleActive(0) : undefined)}
+                className='relative block px-3 py-2 transition hover:text-teal-500'>
+                Home
+                {idActive === 0 && (
+                  <span className='absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0' />
+                )}
+              </NavLink>
+            </li>
+            {menus.slice(0, 7).map((menu) => (
+              <li key={menu.id}>
+                <NavLink
+                  to={`/genre/${menu.id}`}
+                  style={({ isActive }) => (isActive ? handleActive(menu.id) : undefined)}
+                  className='relative block px-3 py-2 transition hover:text-teal-500'>
+                  {menu.name}
+                  {idActive === menu.id && (
+                    <span className='absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0' />
+                  )}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      {isShowMenuMobile && <MenuMobile menus={menus} onClose={() => setIsShowMenuMobile(false)} />}
+    </>
+  )
+}
+
+export default Menu
+
+const MenuSkeleton = () => (
+  <div className='flex flex-1 justify-end md:justify-center fixed top-6 z-10 inset-x-0 mx-auto'>
+    <nav>
+      <ul className='flex justify-center gap-3 items-center rounded-full relative h-10 w-[680px] bg-white/90 px-3 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur'>
+        {[...Array(AppConfig.ITEMS_PER_PAGE - 1)].map((_, index) => (
+          <li key={index}>
+            <div className='h-5 w-20 rounded  bg-gray-200 animate-pulse' />
+          </li>
+        ))}
+      </ul>
+    </nav>
+  </div>
+)
---
+++ src/components/header/MenuMobile.tsx
@@ -0,0 +1,88 @@
+import { lazy, memo, useState } from 'react'
+import { Form, NavLink } from 'react-router-dom'
+import { IGenre } from '~/types/movies'
+
+const Image = lazy(() => import('~/components/base/Image'))
+
+interface IMenuMobileProps {
+  menus: IGenre[]
+  onClose: () => void
+}
+
+const MenuMobile = ({ menus, onClose }: IMenuMobileProps) => {
+  const [keyword, setKeyword] = useState('')
+  return (
+    <>
+      <div className='w-screen h-screen bg-gray-400 opacity-70 absolute top-0 z-10' onClick={onClose} />
+      <div className='absolute h-screen top-0 max-w-sm rounded-xl bg-white shadow-lg z-20 md:hidden'>
+        <header className='rounded-bl-3xl mb-10 flex transform items-center bg-gradient-to-r from-teal-500 to-teal-600 py-8 px-6'>
+          <Image
+            src='/assets/images/logo.png'
+            alt='This is logo'
+            className='ring-4 ring-gray-200 ring-opacity-20'
+            width='80'
+          />
+          <div className='ml-5'>
+            <h1 className='text-lg tracking-wide text-white'>Tran Anh Quang</h1>
+            <p className='text-sm tracking-wider text-gray-300'>Developer</p>
+          </div>
+        </header>
+        <Form action={`/search/${keyword}`} className='px-2 mb-3'>
+          <label className='mb-2 text-sm font-medium text-gray-900 sr-only'>Search</label>
+          <div className='relative'>
+            <input
+              type='search'
+              className='block w-full p-4 pl-10 text-sm text-gray-900 border border-none rounded-lg'
+              placeholder='Search for a movie, tv show, person...'
+              required
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value.trim())}
+            />
+            <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+              <svg
+                aria-hidden='true'
+                className='w-5 h-5 text-gray-500'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+                />
+              </svg>
+            </div>
+          </div>
+        </Form>
+        <ul className='relative pb-5 font-medium'>
+          <li className='text-md flex items-center py-4 text-gray-700 bg-teal-300 px-4'>
+            <NavLink to='/' className='relative block px-3 py-2 transition'>
+              Home
+            </NavLink>
+          </li>
+          {menus.slice(0, 7).map((menu) => (
+            <li key={menu.id} className='text-md flex items-center py-4 text-gray-700 px-4'>
+              <NavLink to={`/genre/${menu.id}`} className='relative block px-3 py-2 transition hover:bg-teal-300'>
+                {menu.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <span className='absolute left-1/2 bottom-10 text-gray-400' onClick={onClose}>
+          <svg
+            className='h-8 w-8'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'>
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M6 18L18 6M6 6l12 12' />
+          </svg>
+        </span>
+      </div>
+    </>
+  )
+}
+
+export default memo(MenuMobile)
