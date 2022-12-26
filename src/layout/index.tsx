1e328917af90888fcfd8ea71adfd0e640461eaaf
Tran Quang <taquang.hskx.2000@gmail.com>
2022-12-26 11:43:08 +0700

    feat: create footer

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,14 +1,18 @@
+import Footer from '~/components/Footer'
 import Header from '~/components/header/Index'
 import LayoutProvider from '~/context/Layout'
 
 const LayoutRouter = ({ children }: { children: JSX.Element }) => {
   return (
-    <>
-      <Header />
-      <LayoutProvider>
-        <main className='container mx-auto mt-8'>{children}</main>
-      </LayoutProvider>
-    </>
+    <div className='flex-col justify-between h-screen mb-auto'>
+      <div>
+        <Header />
+        <LayoutProvider>
+          <main className='container mx-auto mt-8'>{children}</main>
+        </LayoutProvider>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
